56e61b36144ac677f35db52a8bc872e0209682d5
Edmond Lai <kml42638@co.champaign.il.us>
2017-07-25 16:14:33 -0500

    added testing for mix Traffic, bike lane w and w/o adj parking

--- unit_test.py
+++ unit_test.py
@@ -2,6 +2,8 @@ import arcpy
 import os
 import shutil
 import tempfile
+import blts_arcpy as blts
+
 
 class workSpaceFixture(object):
     GDB_NAME = 'Temp.gdb'
@@ -72,21 +74,164 @@ class workSpaceFixture(object):
         print("Complete tearing down")
 
 
+
+class testingModule(object):
     @classmethod
-    def addData(cls):
+    def _addData(cls, field, data):
     # Add data to the field
-        fields_name = [f[0] for f in cls.FC_FIELD]
-        with arcpy.da.InsertCursor(cls.FC_Path, fields_name) as cursor:
-            for row in cls.FC_DATA:
+        fields_name = [f for f in field]
+        with arcpy.da.InsertCursor(workSpaceFixture.FC_Path,
+                                   fields_name) as cursor:
+            for row in data:
                 cursor.insertRow(row)
 
+        del cursor, row
+
+
+    @classmethod
+    def _deleteData(self):
+        arcpy.DeleteRows_management(workSpaceFixture.FC_Path)
+
+
+    @classmethod
+    def mixTrafficVerify(cls):
+        data = []
+        field = ('speed', 'lpd')
+        speed = [20, 25, 30, 35, 40]
+        lpd = [0, 1, 2, 3, 4]
+        expected_score = [1,2,3,4,4,
+                          1,2,3,4,4,
+                          2,3,4,4,4,
+                          3,4,4,4,4,
+                          3,4,4,4,4]
+
+        # add test data
+        for s in speed:
+            for l in lpd:
+                data.append((s,l))
+
+        cls._addData(field, data)
+
+        a = blts.BLTS_Analysis(workSpaceFixture.GDB_Path,
+                               workSpaceFixture.FC_NAME)
+        a.addField()
+        a.setMixTrafficField('speed', 'lpd')
+        a.assignMixTrafficScore()
+
+        cursor = arcpy.SearchCursor(workSpaceFixture.FC_Path)
+        calScore = []
+        for row in cursor:
+            calScore.append(row.getValue("mixTraffic"))
+
+
+        if calScore == expected_score:
+            print("All Mix Traffic score is as expected...")
+        else:
+            print("Calculated score and expected mix traffic score does not match")
+
+        cls._deleteData()
+        del cursor, row, a
+
+
+    @classmethod
+    def bikeLaneWithPkVerify(cls):
+        data = []
+        field = ('hasParkingLane', 'lpd', 'speed', 'combParkingWidth')
+        hasParking = [50, 100]
+        lpd = [0, 1, 2, 3]
+        speed = [20, 25, 30, 35, 40, 45]
+        combParkingWidth = [16, 15, 14, 13, 12]
+        expected_score = [1, 1, 2, 3, 3,
+                          1, 1, 2, 3, 3,
+                          1, 1, 2, 3, 3,
+                          2, 2, 3, 3, 3,
+                          2, 2, 4, 4, 4,
+                          2, 2, 4, 4, 4,] * 2 + \
+                         [2, 2, 3, 3, 3,
+                          2, 2, 3, 3, 3,
+                          2, 2, 3, 3, 3,
+                          3, 3, 3, 3, 3,
+                          3, 3, 4, 4, 4,
+                          3, 3, 4, 4, 4] * 2 + \
+                         [None] * 6 * 5 * 2 * 2
+
+        for h in hasParking:
+            for l in lpd:
+                for s in speed:
+                    for c in combParkingWidth:
+                        data.append((h,l,s,c))
+
+        cls._addData(field, data)
+        a = blts.BLTS_Analysis(workSpaceFixture.GDB_Path,
+                               workSpaceFixture.FC_NAME)
+        a.setPkLaneField("lpd", "speed", "combParkingWidth", "HasParkingLane")
+        a.assignBLwithPkLane()
+
+        cursor = arcpy.SearchCursor(workSpaceFixture.FC_Path)
+        calScore = []
+        for row in cursor:
+            calScore.append(row.getValue("pkLane"))
+
+        if calScore == expected_score:
+            print("All Bike Lane with Adj Parking score is as expected...")
+        else:
+            print("Calculated score and BL w/ parking score does not match")
+
+        cls._deleteData()
+        del a, cursor, row
+
+
+    @classmethod
+    def bikeLaneWithoutPkVerify(cls):
+        data = []
+        field = ('hasParkingLane', 'lpd', 'speed', 'width')
+        hasParking = [50, 100]
+        lpd = [0, 1, 2, 3]
+        speed = [25, 30, 35, 40, 45]
+        width = [8, 7, 6, 5]
+        expected_score = [None] * 80 + \
+                         [1, 1, 1, 2,
+                          1, 1, 1, 2,
+                          2, 2, 3, 3,
+                          3, 3, 4, 4,
+                          3, 3, 4, 4,] * 2 + \
+                          [1, 1, 3, 3,
+                           1, 1, 3, 3,
+                           2, 2, 3, 3,
+                           3, 3, 4, 4,
+                           3, 3, 4, 4] * 2
+
+        for h in hasParking:
+            for l in lpd:
+                for s in speed:
+                    for c in width:
+                        data.append((h,l,s,c))
+
+        cls._addData(field, data)
+        a = blts.BLTS_Analysis(workSpaceFixture.GDB_Path,
+                               workSpaceFixture.FC_NAME)
+        a.setNoPkLaneField("lpd", "speed", "width", "HasParkingLane")
+        a.assignBLwithoutPkLane()
+
+        cursor = arcpy.SearchCursor(workSpaceFixture.FC_Path)
+        calScore = []
+        for row in cursor:
+            calScore.append(row.getValue("noPkLane"))
+
+        if calScore == expected_score:
+            print("All Bike Lane w/o Adj Parking score is as expected...")
+        else:
+            print("Calculated score and BL w/o parking score does not match")
+
+        cls._deleteData()
+        del a, cursor, row
 
 
 def main():
-    try:
         workSpaceFixture.setUpModule()
-    finally:
-        pass
+        testingModule.mixTrafficVerify()
+        testingModule.bikeLaneWithPkVerify()
+        testingModule.bikeLaneWithoutPkVerify()
         #workSpaceFixture.tearDownModule()
 
 
