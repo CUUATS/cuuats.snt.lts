df44bc0fe9c840775a125bb1762927c09282c418
Edmond Lai <kml42638@co.champaign.il.us>
2017-07-31 10:53:06 -0500

    added buffer type, buffer width, landuse

--- plts_arcpy.py
+++ plts_arcpy.py
@@ -1,35 +1,44 @@
 # plts_arcpy.py
 # This script uses the arcpy library to calculate the PLTS score for
 # each sidewalk segment and write the result in ArcGIS feature class
-# Library required: arcpy
+# Licensed library required: arcpy
 
 import arcpy
 import os
-from config import SW_COND_TABLE
+from config import SW_COND_TABLE, BUFFER_TYPE_TABLE, BUFFER_WIDTH_TABLE, \
+    LANDUSE_DICT
 
 
 class PLTS_Analysis(object):
     def __init__(self, GDB_PATH, FC_NAME):
         self.GBD_PATH = GDB_PATH
         self.FC_NAME = FC_NAME
-        self.FULL_PATH = os.path.join(self.GBD_PATH, self.FC_NAME)
-        self.scoreFields = ["swCondScore"]
-        self._setupFields()
+        self.FC_PATH = os.path.join(self.GBD_PATH, self.FC_NAME)
+        self.scoreFields = ["swCondScore",
+                            "buffTypeScore",
+                            "buffWidthScore",
+                            "landuseScore",
+                            "aggregateScore"]
 
-    def _setupFields(self):
-        fc_fields = arcpy.ListFields(self.FULL_PATH)
+
+    def _setupFields(self, inField, checklist):
+        fc_fields = arcpy.ListFields(self.FC_PATH)
         fc_field_list = [field.name for field in fc_fields]
+
+        for field in checklist:
+            if field not in fc_field_list:
+                raise NameError("{} is not found".format(field))
 
         for scoreField in self.scoreFields:
             if scoreField in fc_field_list:
-                with arcpy.da.UpdateCursor(self.FULL_PATH,
+                with arcpy.da.UpdateCursor(self.FC_PATH,
                                            scoreField) as cursor:
                     for row in cursor:
                         row[0] = None
                         cursor.updateRow(row)
 
             else:
-                arcpy.AddField_management(self.FULL_PATH,
+                arcpy.AddField_management(self.FC_PATH,
                                           scoreField,
                                           "SHORT")
 
@@ -38,9 +47,10 @@ class PLTS_Analysis(object):
         if not sw_cond or not sw_width:
             raise ValueError("Sidewalk Width and Sidewalk "
                                 "Condition must be entered")
+        self._setupFields(self.scoreFields[0], checklist=[sw_cond, sw_width])
 
         field_names = [sw_width, sw_cond, self.scoreFields[0]]
-        with arcpy.da.UpdateCursor(self.FULL_PATH, field_names) as cursor:
+        with arcpy.da.UpdateCursor(self.FC_PATH, field_names) as cursor:
             for row in cursor:
                 # Less than 4 feet of sidewalk
                 if row[0] == None:
@@ -94,6 +104,152 @@ class PLTS_Analysis(object):
                         row[2] = SW_COND_TABLE[3][4]
 
                 cursor.updateRow(row)
+
+        del cursor, row
+
+
+    def assignBufferTypeScore(self, buff_type = None, speed = None):
+        if not speed or not buff_type:
+            raise ValueError("Speed and Buffer Type must be entered")
+        self._setupFields(self.scoreFields[1], checklist=[buff_type, speed])
+
+        field_names = [buff_type, speed, self.scoreFields[0]]
+        with arcpy.da.UpdateCursor(self.FC_PATH, field_names) as cursor:
+            for row in cursor:
+                # No Buffer
+                if row[0] == "No Buffer":
+                    if row[1] <= 25:
+                        row[2] = BUFFER_TYPE_TABLE[0][0]
+                    elif row[1] == 30:
+                        row[2] = BUFFER_TYPE_TABLE[0][1]
+                    elif row[1] == 35:
+                        row[2] = BUFFER_TYPE_TABLE[0][2]
+                    else:
+                        row[2] = BUFFER_TYPE_TABLE[0][3]
+                # Solid Surface
+                elif row[1] == "Solid Surface":
+                    if row[1] <= 25:
+                        row[2] = BUFFER_TYPE_TABLE[1][0]
+                    elif row[1] == 30:
+                        row[2] = BUFFER_TYPE_TABLE[1][1]
+                    elif row[1] == 35:
+                        row[2] = BUFFER_TYPE_TABLE[1][2]
+                    else:
+                        row[2] = BUFFER_TYPE_TABLE[1][3]
+                # Landscaped
+                elif row[1] == "Landscaped":
+                    if row[1] <= 25:
+                        row[2] = BUFFER_TYPE_TABLE[2][0]
+                    elif row[1] == 30:
+                        row[2] = BUFFER_TYPE_TABLE[2][1]
+                    elif row[1] == 35:
+                        row[2] = BUFFER_TYPE_TABLE[2][2]
+                    else:
+                        row[2] = BUFFER_TYPE_TABLE[2][3]
+                # Landscaped with tree or Vertical
+                else:
+                    if row[1] <= 25:
+                        row[2] = BUFFER_TYPE_TABLE[3][0]
+                    elif row[1] == 30:
+                        row[2] = BUFFER_TYPE_TABLE[3][1]
+                    elif row[1] == 35:
+                        row[2] = BUFFER_TYPE_TABLE[3][2]
+                    else:
+                        row[2] = BUFFER_TYPE_TABLE[3][3]
+
+            cursor.updateRow(row)
+
+        del cursor, row
+
+
+    def assignBufferWidthScore(self, buff_width = None, total_lanes = None):
+        if not buff_width or not total_lanes:
+            raise ValueError("Buffer Width and Total Lanes must be entered")
+        self._setupFields(self.scoreFields[2], checklist=[buff_width,
+                                                          total_lanes])
+
+        field_names = [total_lanes, buff_width, self.scoreFields[2]]
+        with arcpy.da.UpdateCursor(self.FC_PATH, field_names) as cursor:
+            for row in cursor:
+                # Total 2 lanes
+                if row[0] == 2:
+                    if row[1] < 5:
+                        row[2] = BUFFER_WIDTH_TABLE[0][0]
+                    elif row[1] >= 5 and row[1] < 10:
+                        row[2] = BUFFER_WIDTH_TABLE[0][1]
+                    elif row[1] >= 10 and row[1] < 15:
+                        row[2] = BUFFER_WIDTH_TABLE[0][2]
+                    elif row[1] >= 15 and row[1] < 25:
+                        row[2] = BUFFER_WIDTH_TABLE[0][3]
+                    else:
+                        row[2] = BUFFER_WIDTH_TABLE[0][4]
+                # 3 lanes
+                elif row[0] == 3:
+                    if row[1] < 5:
+                        row[2] = BUFFER_WIDTH_TABLE[1][0]
+                    elif row[1] >= 5 and row[1] < 10:
+                        row[2] = BUFFER_WIDTH_TABLE[1][1]
+                    elif row[1] >= 10 and row[1] < 15:
+                        row[2] = BUFFER_WIDTH_TABLE[1][2]
+                    elif row[1] >= 15 and row[1] < 25:
+                        row[2] = BUFFER_WIDTH_TABLE[1][3]
+                    else:
+                        row[2] = BUFFER_WIDTH_TABLE[1][4]
+                # 4-5 lanes
+                elif row[0] == 4 or row[0] == 5:
+                    if row[1] < 5:
+                        row[2] = BUFFER_WIDTH_TABLE[2][0]
+                    elif row[1] >= 5 and row[1] < 10:
+                        row[2] = BUFFER_WIDTH_TABLE[2][1]
+                    elif row[1] >= 10 and row[1] < 15:
+                        row[2] = BUFFER_WIDTH_TABLE[2][2]
+                    elif row[1] >= 15 and row[1] < 25:
+                        row[2] = BUFFER_WIDTH_TABLE[2][3]
+                    else:
+                        row[2] = BUFFER_WIDTH_TABLE[2][4]
+                # 6 lanes
+                else:
+                    if row[1] < 5:
+                        row[2] = BUFFER_WIDTH_TABLE[3][0]
+                    elif row[1] >= 5 and row[1] < 10:
+                        row[2] = BUFFER_WIDTH_TABLE[3][1]
+                    elif row[1] >= 10 and row[1] < 15:
+                        row[2] = BUFFER_WIDTH_TABLE[3][2]
+                    elif row[1] >= 15 and row[1] < 25:
+                        row[2] = BUFFER_WIDTH_TABLE[3][3]
+                    else:
+                        row[2] = BUFFER_WIDTH_TABLE[3][4]
+
+            cursor.updateRow(row)
+        del cursor, row
+
+
+    def assignLaneUseScore(self, landUse = None):
+        if not landUse:
+            raise ValueError("Land use must be entered")
+        self._setupFields(self.scoreFields[3], checklist=[landUse])
+
+        fields_name = [landUse, self.scoreFields[3]]
+
+
+        with arcpy.da.UpdateCursor(self.FC_PATH, fields_name) as cursor:
+            for row in cursor:
+                row[1] = LANDUSE_DICT["{}".format(row[0])]
+                cursor.updateRow(row)
+
+        del cursor, row
+
+
+    def aggregateScore(self, method = "MAX"):
+        with arcpy.da.UpdateCursor(self.FC_PATH, self.scoreFields) as cursor:
+            for row in cursor:
+                if method == "MAX":
+                    row[-1] = max(row[:-1])
+                elif method == "MIN":
+                    row[-1] = min(row[:-1])
+                cursor.updateRow(row)
+
+        del cursor, row
 
 
 def convertScoreCategory(fc_path, inputField,
