3020f29cebd07bc9177aeccb3b6d2a6935f7ff6a
Edmond Lai <kml42638@co.champaign.il.us>
2017-10-12 15:07:18 -0500

    create a python package

---
+++ cuuats/snt/lts/tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestBLTS(unittest.TestCase):
+    def test_blts(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
+version = '0.1.0'
+
+setup(name='cuuats.snt.lts',
+      version=version,
+      description='Level of Traffic Stress for the Sustainable Neigborhood Toolkit',
+      long_description='\n'.join([open(f).read() for f in [
+          'README.md',
+          'HISTORY.rst'
+      ]]),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Win32 (MS Windows)',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Topic :: Database',
+          'Topic :: Scientific/Engineering :: GIS',
+          'Programming Language :: Python :: 2.7',
+      ],
+      keywords='Sustainable Neigborhood Traffic Stress',
+      author='Edmond Lai',
+      author_email='klai@ccrpc.org',
+      url='https://cuuats.org/',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cuuats', 'cuuats.snt']
+      )
