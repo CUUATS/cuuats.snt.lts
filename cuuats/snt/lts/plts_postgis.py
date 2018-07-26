32f2e129840a75f0d2d165290d0a52f0a9b231a1
Edmond Lai <kml42638@co.champaign.il.us>
2018-07-26 12:11:09 -0500

    adds physical buffer score

--- cuuats/snt/lts/plts_postgis.py
+++ cuuats/snt/lts/plts_postgis.py
@@ -8,10 +8,11 @@ from cuuats.snt.lts import config as c
 class Plts(Lts):
     def __init__(self, **kwargs):
         self.sidewalk = kwargs.get('sidewalk')
+        self.segment = kwargs.get('segment')
 
         self.plts_score = 0
         self.condition_score = 0
-        self.phyiscal_buffer_score = 0
+        self.physical_buffer_score = 0
         self.buffer_width_score = 0
         self.land_use_score = 0
 
@@ -32,11 +33,29 @@ class Plts(Lts):
         self.condition_score = max(self.condition_score, score)
         return(score)
 
+    def _calculate_physical_buffer(self):
+        score = 0
+        score = self._calcuate_score(
+            c.BUFFER_TYPE_TABLE,
+            ['self.sidewalk.buffer_type == "No Buffer"',
+             'self.sidewalk.buffer_type == "Solid Buffer"',
+             'self.sidewalk.buffer_type == "Landscaped"',
+             'True'],
+            ['self.segment.posted_speed <= 25',
+             'self.segment.posted_speed == 30',
+             'self.segment.posted_speed == 35',
+             'True']
+        )
+
+        self.physical_buffer_score = max(self.physical_buffer_score, score)
+        return(score)
+
     def calculate_plts(self):
         self._calculate_condition_score()
 
         self.plts_score = self._aggregate_score(
             self.condition_score,
+            self.physical_buffer_score,
             method = "MAX"
         )
 
@@ -45,6 +64,7 @@ if __name__ == '__main__':
                         buffer_type = 'No Buffer',
                         buffer_width = 0,
                         sidewalk_score = 60)
-    plts = Plts(sidewalk = sidewalk)
+    segment = Segment(posted_speed = 30)
+    plts = Plts(sidewalk = sidewalk, segment = segment)
     plts.calculate_plts()
     import pdb; pdb.set_trace()
