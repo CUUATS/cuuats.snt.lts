759c5000d024501240f972fcd3fae368de236a87
Edmond Lai <kml42638@co.champaign.il.us>
2018-07-27 09:32:17 -0500

    adds left turn lane and crossing without median tests

--- cuuats/snt/lts/tests_postgis.py
+++ cuuats/snt/lts/tests_postgis.py
@@ -4,16 +4,14 @@ from cuuats.snt.lts.model.Segment import Segment
 from cuuats.snt.lts.model.Approach import Approach
 
 class BltsTest(unittest.TestCase):
+    segment = Segment()
+    approaches = [Approach()]
+    Blts = Blts(segment, approaches)
+
     def test_environment_setup(self):
-        segment = Segment()
-        approaches = [Approach()]
-        self.Blts = Blts(segment, approaches)
         self.assertEqual(self.Blts.bike_lane_without_adj_parking_score, 0)
 
     def test_calculate_score(self):
-        segment = Segment()
-        approaches = [Approach()]
-        self.Blts = Blts(segment, approaches)
         # Test calculate score function
         aadt = [500, 1500, 3500]
         lanes_per_direction = [0, 1, 2, 3]
@@ -42,9 +40,6 @@ class BltsTest(unittest.TestCase):
         self.assertEqual(outer_list, score_matrix)
 
     def test_mix_traffic(self):
-        segment = Segment()
-        approaches = [Approach()]
-        self.Blts = Blts(segment, approaches)
         # Test the mix traffic function
         aadt = [500, 1500, 3500]
         lanes_per_direction = [0, 1, 2, 3]
@@ -66,9 +61,6 @@ class BltsTest(unittest.TestCase):
 
     def test_bikelane_with_adj_parking_one_lane(self):
         # Test Bike Facility and Parking Lane Width Filter
-        segment = Segment()
-        approaches = [Approach()]
-        self.Blts = Blts(segment, approaches)
         # Test 1 Lane Per Direction or No lanes per direction
         lanes_per_direction = [None, 1]
         for l in lanes_per_direction:
@@ -96,9 +88,6 @@ class BltsTest(unittest.TestCase):
 
     def test_bikelane_with_adj_parking_one_lane(self):
         # Test Bike Facility and Parking Lane Width Filter
-        segment = Segment()
-        approaches = [Approach()]
-        self.Blts = Blts(segment, approaches)
         # Test 1 Lane Per Direction or No lanes per direction
         lanes_per_direction = [None, 1]
         for l in lanes_per_direction:
@@ -125,9 +114,6 @@ class BltsTest(unittest.TestCase):
             self.assertEqual(outer_list, score_matrix)
 
     def test_bikelane_with_adj_parking_two_lanes(self):
-        segment = Segment()
-        approaches = [Approach()]
-        self.Blts = Blts(segment, approaches)
         self.Blts.segment.lanes_per_direction = 2
         aadt = [500, 3000, 30000, 30001]
         bicycle_facility_width = [10, 7]
@@ -151,9 +137,6 @@ class BltsTest(unittest.TestCase):
         self.assertEqual(outer_list, score_matrix)
 
     def test_calculate_right_turn_lane(self):
-        segment = Segment()
-        approaches = [Approach()]
-        self.Blts = Blts(segment, approaches)
         self.Blts.segment.functional_class = 4
 
         self.Blts.approach.lane_configuration = None
@@ -191,5 +174,67 @@ class BltsTest(unittest.TestCase):
         self.Blts.approach.bike_lane_approach = "Straight"
         self.assertEqual(self.Blts._calculate_right_turn_lane(), 2)
 
+    def test_left_turn_lane(self):
+        self.Blts.segment.function_class = 4
+
+        self.Blts.approach.lane_configuration = None
+        self.Blts.segment.posted_speed = 0
+        self.assertEqual(self.Blts._calculate_left_turn_lane(), 0)
+
+        # Test for dual shared or exclusive left turn lane
+        self.Blts.approach.lane_configuration = "XXLTTR"
+        self.assertEqual(self.Blts._calculate_left_turn_lane(), 4)
+        self.Blts.approach.lane_configuration = "XXKTTR"
+        self.assertEqual(self.Blts._calculate_left_turn_lane(), 4)
+
+        # Test for Lanecrossed criterias
+        self.Blts.approach.lane_configuration = "XXTTR"
+        self.Blts.segment.posted_speed = 25
+        self.Blts.approach.lanes_crossed = 0
+
+
+        lanecrossed = [0, 1, 2]
+        posted_speed = [25, 30, 35]
+        score_matrix = [[2, 2, 3],
+                        [2, 3, 4],
+                        [3, 4, 4]]
+        outer_list = []
+        inner_list = []
+        for p in posted_speed:
+            self.Blts.segment.posted_speed = p
+            for l in lanecrossed:
+                self.Blts.approach.lanes_crossed = l
+                score = self.Blts._calculate_left_turn_lane()
+                inner_list.append(score)
+            outer_list.append(inner_list)
+            inner_list = []
+
+        self.assertEqual(outer_list, score_matrix)
+
+    def test_crossing_without_median(self):
+        self.Blts.approach.lane_configuration = "XXTT"
+        self.Blts.segment.posted_speed = 25
+        self.Blts.approach.total_lanes = 3
+        self.assertEqual(self.Blts._calculate_crossing_without_median(), 1)
+
+        posted_speed = [25, 30, 35, 40]
+        total_lanes = [3, 5, 6]
+        score_matrix = [[1, 2, 4],
+                        [1, 2, 4],
+                        [2, 3, 4],
+                        [3, 4, 4]]
+        outer_list = []
+        inner_list = []
+        for p in posted_speed:
+            self.Blts.segment.posted_speed = p
+            for l in total_lanes:
+                self.Blts.approach.total_lanes = l
+                score = self.Blts._calculate_crossing_without_median()
+                inner_list.append(score)
+            outer_list.append(inner_list)
+            inner_list = []
+        self.assertEqual(outer_list, score_matrix)
+
+    
 if __name__ == '__main__':
     unittest.main()
