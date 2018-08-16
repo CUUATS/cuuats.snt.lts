e43ac2b7e30c81e75ddc0cc2627e6fb580652af9
Edmond Lai <kml42638@co.champaign.il.us>
2018-08-16 12:34:59 -0500

    declare variables at each function for clarity

--- cuuats/snt/lts/model/Segment.py
+++ cuuats/snt/lts/model/Segment.py
@@ -38,86 +38,121 @@ class Segment(object):
         :param self: self
         :return: np.int64 score
         """
-        table = pd.DataFrame(c.MIXED_TRAF_TABLE)
-        crits = ([self.aadt, c.URBAN_FIX_TRAFFIC_AADT_SCALE],
-                 [self.lanes_per_direction, c.URBAN_FIX_TRAFFIC_LANE_SCALE])
+        aadt = self.aadt
+        lpd = self.lanes_per_direction
+        mix_traffic_score = c.MIXED_TRAF_TABLE
+        aadt_scale = c.URBAN_FIX_TRAFFIC_AADT_SCALE
+        lane_scale = c.URBAN_FIX_TRAFFIC_LANE_SCALE
+
+        table = pd.DataFrame(mix_traffic_score)
+        crits = ([aadt, aadt_scale],
+                 [lpd, lane_scale])
         return Lts.calculate_score(table, crits)
 
     def _calculate_bikelane_with_adj_parking(self, bike_paths):
-        if self.parking_lane_width is None:
+        parking_lane_width = self.parking_lane_width
+        lpd = self.lanes_per_direction
+        aadt = self.aadt
+        bl_adj_pk_score_one_lane = c.BL_ADJ_PK_TABLE_ONE_LANE
+        bl_adj_pk_score_two_lanes = c.BL_ADJ_PK_TABLE_TWO_LANES
+        aadt_scale = c.BL_ADJ_PK_AADT_SCALE
+        width_scale = c.BL_ADJ_PK_WIDTH_SCALE
+        width_scale_two_lanes = c.BL_ADJ_PK_TWO_WIDTH_SCALE
+
+        if parking_lane_width is None:
             return 99
 
         for bike_path in bike_paths:
-            if bike_path.width is None:
+            width = bike_path.width
+            if width is None:
                 return 0
 
             # No marked lanes or 1 lpd
-            if self.lanes_per_direction is None or \
-               self.lanes_per_direction == 1:
-                table = pd.DataFrame(c.BL_ADJ_PK_TABLE_ONE_LANE)
-                crits = ([self.aadt, c.BL_ADJ_PK_AADT_SCALE],
-                         [bike_path.width, c.BL_ADJ_PK_WIDTH_SCALE])
+            if lpd is None or lpd == 1:
+                table = pd.DataFrame(bl_adj_pk_score_one_lane)
+                crits = ([aadt, aadt_scale],
+                         [width, width_scale])
                 return (Lts.calculate_score(table, crits))
             # 2 lpd or greater
             else:
-                table = pd.DataFrame(c.BL_ADJ_PK_TABLE_TWO_LANES)
-                crits = ([self.aadt, c.BL_ADJ_PK_AADT_SCALE],
-                         [bike_path.width, c.BL_ADJ_PK_TWO_WIDTH_SCALE])
+                table = pd.DataFrame(bl_adj_pk_score_two_lanes)
+                crits = ([aadt, aadt_scale],
+                         [width, width_scale_two_lanes])
                 return (Lts.calculate_score(table, crits))
 
     def _calculate_bikelane_without_adj_parking(self, bike_paths):
-        if self.parking_lane_width is None:
+        parking_lane_width = self.parking_lane_width
+        lpd = self.lanes_per_direction
+        aadt = self.aadt
+        bl_wo_adj_pk_score_one_lane = c.BL_NO_ADJ_PK_TABLE_ONE_LANE
+        bl_wo_adj_pk_score_two_lanes = c.BL_NO_ADJ_PK_TABLE_TWO_LANES
+        aadt_scale = c.BL_NO_ADJ_PK_AADT_SCALE
+        width_scale_one_lane = c.BL_NO_ADJ_PK_WIDTH_SCALE
+        width_scale_two_lane = c.BL_NO_ADJ_PK_TWO_WIDTH_SCALE
+
+        if parking_lane_width is None:
             for bike_path in bike_paths:
-                if bike_path.width is None:
+                width = bike_path.width
+                if width is None:
                     return 0
                 # no marked lane or 1 lpd
-                if self.lanes_per_direction is None or \
-                   self.lanes_per_direction == 1:
-                    table = pd.DataFrame(c.BL_NO_ADJ_PK_TABLE_ONE_LANE)
-                    crits = ([self.aadt, c.BL_NO_ADJ_PK_AADT_SCALE],
-                             [bike_path.width, c.BL_NO_ADJ_PK_WIDTH_SCALE])
+                if lpd is None or lpd == 1:
+                    table = pd.DataFrame(bl_wo_adj_pk_score_one_lane)
+                    crits = ([aadt, aadt_scale],
+                             [width, width_scale_one_lane])
                     return (Lts.calculate_score(table, crits))
                 # 2 lps or greater
                 else:
-                    table = pd.DataFrame(c.BL_NO_ADJ_PK_TABLE_TWO_LANES)
-                    crits = ([self.aadt, c.BL_NO_ADJ_PK_AADT_SCALE],
-                             [bike_path.width, c.BL_NO_ADJ_PK_TWO_WIDTH_SCALE])
+                    table = pd.DataFrame(bl_wo_adj_pk_score_two_lanes)
+                    crits = ([aadt, aadt_scale],
+                             [width, width_scale_two_lane])
                     return (Lts.calculate_score(table, crits))
 
     def _calculate_right_turn_lane(self, approach):
-        if approach.lane_configuration is None or \
+        lane_config = approach.lane_configuration
+        rtl_length = approach.right_turn_lane_length
+        bike_lane_approach = approach.bike_lane_approach
+        rtl_score = c.RTL_CRIT_TABLE
+        straight = "Straight"
+
+        if lane_config is None or \
            self.functional_class is None:
             return 0
 
-        if "R" in approach.lane_configuration:
-            if approach.right_turn_lane_length <= 150 and \
-               approach.bike_lane_approach is "Straight":
-                return c.RTL_CRIT_TABLE[0]
-            elif approach.right_turn_lane_length > 150 and \
-                 approach.bike_lane_approach is "Straight":
-                return c.RTL_CRIT_TABLE[1]
+        if "R" in lane_config:
+            if rtl_length <= 150 and bike_lane_approach is straight:
+                return rtl_score[0]
+            elif rtl_length > 150 and bike_lane_approach is straight:
+                return rtl_score[1]
             else:
-                return c.RTL_CRIT_TABLE[2]
-        elif "Q" in approach.lane_configuration:
-            return c.RTL_CRIT_TABLE[3]
+                return rtl_score[2]
+        elif "Q" in lane_config:
+            return rtl_score[3]
 
         return 0
 
     def _calculate_left_turn_lane(self, approach):
         lane_config = approach.lane_configuration
         speed = self.posted_speed
+        functional_class = self.functional_class
+        K = "K"  # dual shared
+        L = "L"  # exclusive left turn lane
+        dual_shared_score = c.LTL_DUAL_SHARED_TABLE
+        ltl_score = c.LTL_CRIT_TABLE
+        speed_scale = c.LTL_DUAL_SHARED_SPEED_SCALE
+        lane_crossed_scale = c.LTL_CRIT_LANE_CROSSED_SCALE
 
-        if lane_config is None or self.functional_class is None:
+        if lane_config is None or functional_class is None:
             return 0
 
-        if "K" in lane_config or "L" in lane_config:
-            table = pd.Series(c.LTL_DUAL_SHARED_TABLE)
-            crit = [(speed, c.LTL_DUAL_SHARED_SPEED_SCALE)]
+        if L in lane_config or K in lane_config:
+            table = pd.Series(dual_shared_score)
+            crit = [(speed, speed_scale)]
             return Lts.calculate_score(table, crit)
         else:
-            table = pd.DataFrame(c.LTL_CRIT_TABLE)
-            crit = [(speed, c.LTL_CRIT_SPEED_SCALE),
-                    (speed, c.LTL_CRIT_LANE_CROSSED_SCALE)]
+            table = pd.DataFrame(ltl_score)
+            crit = [(speed, speed_scale),
+                    (speed, lane_crossed_scale)]
             return Lts.calculate_score(table, crit)
 
     def blts_score(self, approaches, bike_paths=None, turn_threshold=0):
