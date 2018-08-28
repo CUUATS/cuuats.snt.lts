6d68bc1ac1895b49cdb7626d7c90c1e716dbe899
Edmond Lai <kml42638@co.champaign.il.us>
2018-08-28 10:04:20 -0500

    adds sql to create plts view and break up sidewalk segment

---
+++ cuuats/snt/lts/sql/plts_view.sql
@@ -0,0 +1,66 @@
+CREATE OR REPLACE FUNCTION
+set_plts(score_condition float,
+		 sidewalk_width float,
+		 posted_speed int,
+		 buffer_type char,
+		 total_lanes int,
+		 buffer_width int,
+		 overall_landuse char
+		) RETURNS INT AS
+'
+from cuuats.snt.lts.model.Segment import Segment
+from cuuats.snt.lts.model.Sidewalk import Sidewalk
+	
+segment = Segment(total_lanes=total_lanes,
+				  posted_speed=posted_speed,
+				  overall_landuse=overall_landuse)
+sidewalks = [Sidewalk(sidewalk_width=sidewalk_width,
+					  buffer_type=buffer_type,
+					  buffer_width=buffer_width,
+					  sidewalk_score=score_condition)]
+
+score = segment.plts_score(sidewalks)
+return score
+'
+LANGUAGE 'plpython3u';
+
+DROP MATERIALIZED VIEW street.plts_mat_view;
+CREATE MATERIALIZED VIEW street.plts_mat_view AS
+SELECT s.id,
+		s.name,
+		s.geom,
+		s.idot_aadt,
+		s.posted_speed,
+		s.parking_lane_width,
+		s.functional_classification,
+		max(set_plts(score_condition,
+					 sw_width,
+					 posted_speed,
+					 buffer_type,
+					 total_lanes,
+					 buffer_width,
+					 overall_land_use)) as plts
+FROM street.segment AS s
+LEFT JOIN pedestrian.sidewalk_singlepart as w
+	ON ST_DWithin(s.geom, w.sw_geom, 100) AND
+	  pcd_segment_match(s.geom, w.sw_geom, 100)
+GROUP BY s.id,
+		s.name,
+		s.geom,
+		s.idot_aadt,
+		s.posted_speed,
+		s.parking_lane_width,
+		s.lanes_per_direction,
+		s.functional_classification;
+	
+-- update pedestrian.sidewalk_segment set buffer_type = 'landscaped with trees' where buffer_type = 'Landscaped with Trees';
+
+DROP MATERIALIZED VIEW street.lts_view;
+CREATE MATERIALIZED VIEW street.lts_view AS
+SELECT s.id, s.geom , b.blts, p.plts 
+FROM street.segment as s
+LEFT JOIN street.blts_mat_view as b
+	ON s.id = b.id
+LEFT JOIN street.plts_view as p
+	ON s.id = p.id
+
---
+++ cuuats/snt/lts/sql/sidewalk_singlepart.sql
@@ -0,0 +1,12 @@
+CREATE MATERIALIZED VIEW pedestrian.sidewalk_singlepart AS
+SELECT
+	s.id, 
+	(st_dump(s.geom)).geom AS sw_geom,
+	path_type as sw_type,
+	width as sw_width,
+	score_condition,
+	buffer_type,
+	buffer_width,
+	segment_id
+FROM pedestrian.sidewalk_segment as s;
+
