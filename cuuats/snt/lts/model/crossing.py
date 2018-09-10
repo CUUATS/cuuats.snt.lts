15d615427b1a82ee5e58728a64bd5931b6cb4000
Edmond Lai <kml42638@co.champaign.il.us>
2018-09-10 12:27:27 -0500

    update crossing to handle null value

--- cuuats/snt/lts/model/crossing.py
+++ cuuats/snt/lts/model/crossing.py
@@ -3,7 +3,7 @@
 
 class Crossing:
     def __init__(self, **kwargs):
-        self.crossing_speed = kwargs.get('crossing_speed')
-        self.control_type = kwargs.get('control_type')
-        self.lanes = kwargs.get('lanes')
+        self.crossing_speed = kwargs.get('crossing_speed') or 0
+        self.control_type = kwargs.get('control_type') or None
+        self.lanes = kwargs.get('lanes') or 0
         self.median = kwargs.get('median') or None
