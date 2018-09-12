d93d06bb5e399903b1144844aa53ee0339b61859
Edmond Lai <kml42638@co.champaign.il.us>
2018-09-12 14:23:05 -0500

    create sql to query start_intersection, end_intersection, travel_time and frequencey.

--- cuuats/snt/lts/sql/transit_network.sql
+++ cuuats/snt/lts/sql/transit_network.sql
@@ -16,3 +16,37 @@ FROM (
     FROM gtfs.stop_times
 ) edge
 GROUP BY edge.seq
+
+
+-- Create view with intersection id join to bus stop id
+CREATE OR REPLACE VIEW gtfs.int_stop AS
+SELECT s.stop_id,
+  (SELECT i.intersection_id
+   FROM street.intersection as i
+   ORDER BY s.geom <#> i.shape LIMIT 1) AS intersection_id
+FROM gtfs.stops AS s;
+
+-- Create intersection nodes with time between and count
+CREATE MATERIALIZED VIEW gtfs.transit_edge
+SELECT
+    edge.start_intersection,
+    edge.end_intersection,
+    avg(edge.time_diff),
+    count(edge.seq)
+FROM
+    (SELECT
+    	lag(intersection_id)
+    		OVER (PARTITION BY trip_id
+                    ORDER BY stop_sequence)
+                        AS start_intersection,
+        intersection_id AS end_intersection,
+        arrival_time::interval -
+        lag(arrival_time)
+            OVER (PARTITION BY trip_id
+                    ORDER BY stop_sequence)::interval
+                        AS time_diff
+    FROM gtfs.stop_times AS stop_times
+    JOIN gtfs.int_stop AS int_stop
+        ON int_stop.stop_id =  stop_times.stop_id
+) edge
+GROUP BY edge.start_intersection, edge.end_intersection
